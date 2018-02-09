42aba1df1db719f1b05ef2c08d1aeefd14623806
Igor <IHateWin7@gmail.com>
2018-02-09 15:31:37 +0700

    Инициализация интерфейса решения

---
+++ slae-project/slae-project/Solver/solver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using slae_project.Matrix;
+
+namespace slae_project.Solver
+{
+    interface solver
+    {
+        Vector solve(IMatrix A, Vector b, double precision, int maxiter);
+    }
+}
