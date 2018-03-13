9dc5441276b70467a64f2f0a6dfe4e8227f07173
LittleAngryFox <elena.ok35@mail.ru>
2018-03-13 20:49:57 +0700

    Изменение, дополнение, попытка изменить мир к лучшему

--- slae-project/slae-project/Solver/LOSSolver.cs
+++ slae-project/slae-project/Solver/LOSSolver.cs
@@ -1,4 +1,6 @@
-﻿using slae_project.Matrix;
+﻿using slae_project.Logger;
+using slae_project.Matrix;
+using slae_project.Preconditioner;
 using slae_project.Vector;
 using System;
 using System.Collections.Generic;
@@ -19,33 +21,94 @@ namespace slae_project.Solver
         /// <param name="Precision">Точность</param>
         /// <param name="Maxiter">Максимальное число итераций</param>
         /// <returns>Вектор x - решение СЛАУ Ax=b с заданной точностью</returns>
-        public IVector Solve(IMatrix A, IVector b, IVector Initial, double Precision, int Maxiter)
+        /// 
+        public IVector Solve(IPreconditioner A, IMatrix AA, IVector b, IVector Initial, double Precision, int Maxiter, ILogger Logger)
         {
             IVector x = new SimpleVector(b.Size);
 
             if (b.Norm == 0)
                 return x;
 
-            double alpha, beta = 1.0;
+            double alpha = 0.0, beta = 0.0;
 
-            IVector r = b.Add(A.Mult(Initial), 1, -1); //r_0 = f - Ax_0
-            IVector Ar, z = r; // z_0 = r_0
-            IVector p = A.Mult(z); // p_0 = Az_0
+            IVector r = b.Add(AA.Mult(Initial), 1, -1); //r_0 = f - Ax_0
+            r = A.SSolve(r); // r_0 = L^-1 * (f - Ax_0)
+
+            IVector Ar, z = A.QSolve(r); // z_0 = U^-1 * r_0
+            IVector p = A.SSolve(AA.Mult(z)); // p_0 = L^-1 * Az_0
+
             double p_r = 0.0, p_p = 0.0;
 
-            for (int iter = 0; iter < Maxiter && r.ScalarMult(r) > Precision && beta > 0; iter++)
+            double scalRR = r.ScalarMult(r);
+            double normR = Math.Sqrt(scalRR) / b.Norm;
+
+            for (int iter = 0; iter < Maxiter && normR > Precision; iter++)
+            //for (int iter = 0; iter < Maxiter && ; iter++)
+            {
+                p_r = p.ScalarMult(r); //(p_k-1,r_k-1)
+                p_p = p.ScalarMult(p); //(p_k-1,p_k-1)
+                alpha = p_r / p_p;
+
+                x.Add(z, 1, alpha, true); // x_k = x_k-1 + alfa_k*z_k-1
+
+                r.Add(p, 1, -alpha, true); // r_k = r_k-1 - alfa_k*p_k-1
+
+                // Ar = A.QSolve(AA.Mult(A.SSolve(r))); //Ar_k = L^-1 * A * U^-1 * r_k
+                Ar = A.QSolve(r);
+                Ar = AA.Mult(Ar);
+                Ar = A.SSolve(Ar);
+
+                beta = -(p.ScalarMult(Ar) / p_p);
+
+                z = A.QSolve(r).Add(z, 1, beta); //z_k = U^-1 * r_k + beta_k*z_k-1
+                p = Ar.Add(p, 1, beta); // p_k = L^-1 * A * U^-1 * r_k + beta_k*p_k-1
+                if (scalRR == 0) throw new Exception("Division by 0");
+                scalRR = r.ScalarMult(r);
+                normR = Math.Sqrt(scalRR) / b.Norm;
+            }
+            return x;
+        }
+
+        public IVector Solve2(IPreconditioner A, IMatrix AA, IVector b, IVector Initial, double Precision, int Maxiter, ILogger Logger)
+        {
+            IVector x = new SimpleVector(b.Size);
+
+            if (b.Norm == 0)
+                return x;
+
+            double alpha = 0.0, beta = 0.0;
+
+            IVector r = b.Add(AA.Mult(Initial), 1, -1); //r_0 = f - Ax_0
+            IVector Ar, z = r; // z_0 = r_0
+            IVector p = AA.Mult(z); // p_0 = Az_0
+            double p_r = 0.0, p_p = 0.0;
+            double scal = 1.0;/* p_r - alpha * alpha * p_p;*/
+
+            double scalRR = r.ScalarMult(r);
+            double normR = Math.Sqrt(scalRR) / b.Norm;
+
+            for (int iter = 0; iter < Maxiter && normR > Precision; iter++)
+            //for (int iter = 0; iter < Maxiter && ; iter++)
             {
                 p_r = p.ScalarMult(r); //(p_k-1,r_k-1)
                 p_p = p.ScalarMult(p); //(p_k-1,p_k-1)
                 alpha = p_r / p_p;
                 x.Add(z, 1, alpha, true); // x_k = x_k-1 = alfa_k*z_k-1
+
+                scal = p_r - alpha * alpha * p_p;
+
                 r.Add(p, 1, -alpha, true); // r_k = r_k-1 - alfa_k*p_k-1
-                Ar = A.Mult(r); // Ar_k
+                Ar = AA.Mult(r); // Ar_k
                 beta = -(p.ScalarMult(Ar) / p_p);
                 z = r.Add(z, 1, beta); //z_k = r_k + beta_k*z_k-1
                 p = Ar.Add(p, 1, beta); // p_k = Ar_k + beta_k*p_k-1
+                if (scalRR == 0) throw new Exception("Division by 0");
+                scalRR = r.ScalarMult(r);
+                normR = Math.Sqrt(scalRR) / b.Norm;
             }
             return x;
         }
+
+
     }
 }
