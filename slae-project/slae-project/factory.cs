e2eb6d32fedabf81da768da648c368b5272be090
Tania540 <tan.drobinina@mail.ru>
2018-03-22 17:34:27 +0700

    Убрала лишний size, обнулила словарь,чтобы не накапливал, считывание вектора исправила

--- slae-project/slae-project/factory.cs
+++ slae-project/slae-project/factory.cs
@@ -16,24 +16,25 @@ namespace slae_project
     {
         Form1 main_form;
         FileLogger Log = new FileLogger();
-
         public static Dictionary<string, string> DictionaryOfFormats = Form2.filenames_format;//словарь путей до массивов
-        static public Dictionary<string, Func <Dictionary<string, string>, IMatrix>> MatrixTypes = new Dictionary<string, Func<Dictionary<string, string>, IMatrix>>();
+        static public Dictionary<string, (Func<Dictionary<string, string>, IMatrix> ,Dictionary<string, string>)> MatrixTypes = new Dictionary<string, (Func<Dictionary<string, string>, IMatrix>, Dictionary<string, string>)>();
         public static IMatrix ObjectOfIMatrix;
         public static IVector Result;
-        static public Dictionary<string, Func <string, IMatrix, IVector, IVector, double, int, FileLogger, IVector>> SolverTypes = new Dictionary<string, Func<string, IMatrix, IVector, IVector, double, int, FileLogger, IVector>>();
-        static public List <string> PrecondTypes = new List<string>();
-        
+        public static List<string> name_arr = new List<string>();
+        public static IPreconditioner Prec = new NoPreconditioner();
+        static public Dictionary<string, Func<IPreconditioner, IMatrix, IVector, IVector, double, int, FileLogger, IVector>> SolverTypes = new Dictionary<string, Func<IPreconditioner, IMatrix, IVector, IVector, double, int, FileLogger, IVector>>();
+        static public List<string> PrecondTypes = new List<string>();
+
         public void RegisterPrecondClass(string Name)
-         {
-             PrecondTypes.Add(Name);
-         }
-        static public void RegisterMatrixClass(string Name, Func<Dictionary<string, string>, IMatrix> Creator1)
         {
-            MatrixTypes.Add(Name, Creator1);
+            PrecondTypes.Add(Name);
+        }
+        static public void RegisterMatrixClass(string Name, Func<Dictionary<string, string>, IMatrix> Creator1, Dictionary<string, string> Creator2)
+        {
+            MatrixTypes.Add(Name, (Creator1, Creator2));
         }
 
-        public void RegisterSolverClass(string Name, Func<string, IMatrix, IVector, IVector, double, int, FileLogger, IVector> Creator1)
+        public void RegisterSolverClass(string Name, Func<IPreconditioner, IMatrix, IVector, IVector, double, int, FileLogger, IVector> Creator1)
         {
             SolverTypes.Add(Name, Creator1);
         }
@@ -44,44 +45,52 @@ namespace slae_project
             RegisterPrecondClass("Методом Зейделя");
             RegisterPrecondClass("LU-разложение");
 
-            RegisterMatrixClass("Координатный", (Dictionary<string, string> DictionaryOfFormats) => new CoordinateMatrix.requiredFileNames(DictionaryOfFormats));
-            RegisterMatrixClass("Плотный", (Dictionary<string, string> DictionaryOfFormats) => new DenseMatrix.requiredFileNames(DictionaryOfFormats));
-           // RegisterMatrixClass("Строчный", (Dictionary<string, string> DictionaryOfFormats) => new SparseRowMatrix(DictionaryOfFormats));
-            RegisterMatrixClass("Строчно - столбцовый", (Dictionary<string, string> DictionaryOfFormats) => new SparseRowColumnMatrix.requiredFileNames(DictionaryOfFormats));
+            RegisterMatrixClass("Координатный", (Dictionary<string, string> DictionaryOfFormats) => new CoordinateMatrix(DictionaryOfFormats), CoordinateMatrix.requiredFileNames);
+            RegisterMatrixClass("Плотный", (Dictionary<string, string> DictionaryOfFormats) => new DenseMatrix(DictionaryOfFormats), DenseMatrix.requiredFileNames);
+            //RegisterMatrixClass("Строчный", (Dictionary<string, string> DictionaryOfFormats) => new SparseRowMatrix(DictionaryOfFormats),SparseRowMatrix.requiredFileNames);
+            RegisterMatrixClass("Строчно - столбцовый", (Dictionary<string, string> DictionaryOfFormats) => new SparseRowColumnMatrix(DictionaryOfFormats), SparseRowColumnMatrix.requiredFileNames);
 
-            RegisterSolverClass("Метод сопряжённых градиентов", (string a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new MSGSolver.Solver(Form1.str_precond, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
-            RegisterSolverClass("Локально-оптимальная схема", (string a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new LOSSolver.Solver(Form1.str_precond, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
-            //RegisterSolverClass("Метод Якоби", (string a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new Jacoby.Solver(Form1.str_precond, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
-            //RegisterSolverClass("Метод Зейделя", (string a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new Zeid.Solver(Form1.str_precond, ObjectOfIMatrix, Form2.F,  Form2.X0,Form1.s_accur_number, Form1.max_iter, Log));
-            RegisterSolverClass("Метод бисопряжённых градиентов", (string a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new BSGStabSolve.Solver(Form1.str_precond, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
-            //RegisterSolverClass("Метод обобщённых минимальных невязок", (string a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new SparseRowColumnMatrix.Solver(Form1.str_precond, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
+            ISolver Msg = new MSGSolver();
+            ISolver Los = new LOSSolver();
+            ISolver Bsg = new BSGStabSolve();
+
+            RegisterSolverClass("Метод сопряжённых градиентов", (IPreconditioner a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => Msg.Solve(Prec, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
+            RegisterSolverClass("Локально-оптимальная схема", (IPreconditioner a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => Los.Solve(Prec, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
+            //RegisterSolverClass("Метод Якоби", (IPreconditioner a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new Jacoby.Solver(Prec, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
+            //RegisterSolverClass("Метод Зейделя", (IPreconditioner a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new Zeid.Solver(Prec, ObjectOfIMatrix, Form2.F,  Form2.X0,Form1.s_accur_number, Form1.max_iter, Log));
+            RegisterSolverClass("Метод бисопряжённых градиентов", (IPreconditioner a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => Bsg.Solve(Prec, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
+            //RegisterSolverClass("Метод обобщённых минимальных невязок", (IPreconditioner a, IMatrix b, IVector c, IVector d, double e, int f, FileLogger g) => new SparseRowColumnMatrix.Solver(Prec, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, Log));
 
         }
 
         static public void CreateMatrix(string typename)//получаем заполненную матрицу для передачи Solver
         {
-            Func<Dictionary<string, string>, IMatrix> value;
+            (Func<Dictionary<string, string>, IMatrix>, Dictionary<string, string>) value;
             MatrixTypes.TryGetValue(typename, out value);
-            ObjectOfIMatrix = value(DictionaryOfFormats);
+            Dictionary<string, string> reer;
+            reer = value.Item2;
+            name_arr.Clear();
+            foreach (string i in reer.Keys)
+                     name_arr.Add(i);
+        }
+        static public void Create_Full_Matrix(string typename)//получаем заполненную матрицу для передачи Solver
+        {
+            (Func<Dictionary<string, string>, IMatrix>, Dictionary<string, string>) value;
+            MatrixTypes.TryGetValue(typename, out value);
 
+            ObjectOfIMatrix = value.Item1(DictionaryOfFormats);
         }
         static public void CreateSolver(string typename)//получаем заполненную матрицу для передачи Solver
         {
-            Func<string, IMatrix, IVector, IVector, double, int, FileLogger, IVector> value;
+            Func<IPreconditioner, IMatrix, IVector, IVector, double, int, FileLogger, IVector> value;
             SolverTypes.TryGetValue(typename, out value);
-            Result = value(Form1.str_precond, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, new FileLogger());
-              
+            Result = value(Prec, ObjectOfIMatrix, Form2.F, Form2.X0, Form1.s_accur_number, Form1.max_iter, new FileLogger());
         }
-        /* static public List<string> GetArrays(string typename)//передача нам массивов от форматов матричек
-         {
-             return Matrix.Format_array(typename);
-         }*/
-
         // Мы передаем симметричность/ несимметричность
         public static bool Get_format()
         {
             return Form1.property_matr;
         }
-        
+
     }
 }
