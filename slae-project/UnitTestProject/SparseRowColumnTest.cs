b49997d4c553d7102c7a6edfac3dd02cd75f4cef
Анастасия Матвеева <ansmatv96@gmail.com>
2018-03-22 13:10:47 +0700

    начали делать строчно-столбцовые тесты

---
+++ slae-project/UnitTestProject/SparseRowColumnTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class SparseRowColumnTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
