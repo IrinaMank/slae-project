e2c67c275dcc5805d0caac20c67d374fe0ad9fa3
IrinaMank <zapirinamank@gmail.com>
2018-03-30 21:08:52 +0700

    More logger in BSGstab

--- slae-project/slae-project/Solver/BSGStabSolve.cs
+++ slae-project/slae-project/Solver/BSGStabSolve.cs
@@ -24,6 +24,9 @@ namespace slae_project.Solver
 
         public IVector Solve(IPreconditioner Preconditioner, IMatrix A, IVector b, IVector Initial, double Precision, int Maxiter, ILogger Logger)
         {
+            Logger.WriteNameSolution("BSGstab", Preconditioner.getName());
+            string start = DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss:fff");
+
             Logger.setMaxIter(Maxiter);
             IVector x = (IVector)Initial.Clone();
 
@@ -82,6 +85,7 @@ namespace slae_project.Solver
             }
             x = Preconditioner.SolveU(x);//x = U(-1)x
             Logger.WriteSolution(x, Maxiter);
+            Logger.WriteTime(start, DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss:fff"));
             return x;
         }
     }
